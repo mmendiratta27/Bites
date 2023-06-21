0396e7090ad20faa7a6c0c7ddf4b57675c32d742
jesshuangg <jeshuang@g.hmc.edu>
2023-06-21 14:27:47 -0700

    addPost page works with navigation

--- App.js
+++ App.js
@@ -1,33 +1,54 @@
-import React from 'react';
-import ScreenHeaderBtn from "./src/headerInfo/ScreenHeaderBtn";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import { icons, images } from "./constants";
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './src/home/homeScreen';
-import FeedScreen from './src/feed/Feed';
-import WelcomeScreen from './src/headerInfo/welcome/Welcome';
+import ScreenHeaderBtn from "./src/headerInfo/ScreenHeaderBtn";
+import HomeScreen from "./src/home/homeScreen";
+import FeedScreen from "./src/feed/Feed";
+import WelcomeScreen from "./src/headerInfo/welcome/Welcome";
+import AddPost from "./src/create-posts/addPost";
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer independent={true}>
+    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-        options={{
-                  headerStyle: { backgroundColor: "#F4EEE0" },
-                  headerShadowVisible: false,
-                  headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-                  ),
-                  headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={images.profile} dimension="90%" />
-                  ),
-                  headerTitle: "Munch Hour",
-                }}
-        name='homeScreen' component={HomeScreen} />
-        <Stack.Screen name='Feed' component={FeedScreen} />
-        <Stack.Screen name='Welcome' component={WelcomeScreen} />
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            headerStyle: { backgroundColor: "#F4EEE0" },
+            headerShadowVisible: false,
+            headerRight: () => (
+              <ScreenHeaderBtn
+                iconUrl={images.profile}
+                dimension="90%"
+                // handlePress={() => navigation.navigate("Profile")}
+              />
+            ),
+            headerTitle: "Munch Hour",
+          }}
+        />
+        <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen
+          name="AddPost"
+          component={AddPost}
+          options={({ navigation }) => ({
+            headerStyle: { backgroundColor: "#F4EEE0" },
+            headerShadowVisible: false,
+            headerLeft: () => (
+              <ScreenHeaderBtn
+                iconUrl={icons.left}
+                dimension="60%"
+                handlePress={() => navigation.navigate("HomeScreen")}
+              />
+            ),
+            headerTitle: "Add Post",
+          })}
+        />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
