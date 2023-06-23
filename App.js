968cb1d39e310bae3bcf521b856e12e526b78e40
Manan Mendiratta <mananmendiratta@Manans-MacBook-Pro-3.local>
2023-06-23 15:42:57 -0700

    cleaner code

--- App.js
+++ App.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ScreenHeaderBtn from "./src/headerInfo/ScreenHeaderBtn";
 import { icons, images } from "./constants";
-import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Icons from "react-native-vector-icons/MaterialIcons";
-import LoginScreen from './src/Auth/Login';
-import RegisterScreen from './src/Auth/Register';
+
+//Screens
 import HomeScreen from './src/home/homeScreen';
-import Settings from './src/settings/settings';
-import NotifSettings from './src/settings/notifSettings';
-import ChatScreen from './src/messaging/Chat';
-import ChatHome from './src/messaging/ChatHome';
 import AddPost from "./src/create-posts/addPost";
-import Home from './src/home/homeScreen';
 import SettingsNav from './src/settings/settingsNav';
 import ChatNav from './src/messaging/ChatNav'
 
-
-
-// const Stack = createStackNavigator();
 const homeName = 'Home'
 const addpostName = "Add Post"
 const settingsName = 'Settings'
@@ -104,55 +95,7 @@ const App = () => {
           headerTitle: "Settings",        
         }}
         />
-       
-
       </Tab.Navigator>
-
-
-
-
-
-      {/* <Stack.Navigator screenOptions={{ animation: false }}>
-        <Stack.Screen name='Login' component={LoginScreen} />
-        <Stack.Screen name='Register' component={RegisterScreen} />
-        <Stack.Screen
-
-        options={{
-                  headerStyle: { backgroundColor: "#F4EEE0" },
-                  headerShadowVisible: false,
-                  headerLeft: () => (
-                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
-                  ),
-                  headerRight: () => (
-                    <ScreenHeaderBtn iconUrl={images.profile} dimension="90%" />
-                  ),
-                  headerTitle: "Munch Hour",
-                }}
-        name='homeScreen' component={HomeScreen} />
-        <Stack.Screen name = 'Settings' component = {Settings} options={{ headerShown: false}}/>
-        <Stack.Screen name='notifSettings' component={NotifSettings} options={{headerShown: false}}/>
-        <Stack.Screen name='Chat' component={ChatScreen}
-        options={({ route }) => ({ title: route.params.thread.name })}
-        />
-        <Stack.Screen name="ChatHome" component={ChatHome} />
-        <Stack.Screen
-                  name="AddPost"
-                  component={AddPost}
-                  options={({ navigation }) => ({
-                    headerStyle: { backgroundColor: "#F4EEE0" },
-                    headerShadowVisible: false,
-                    headerLeft: () => (
-                      <ScreenHeaderBtn
-                        iconUrl={icons.left}
-                        dimension="60%"
-                        handlePress={() => navigation.navigate("homeScreen")}
-                      />
-                    ),
-                    headerTitle: "Add Post",
-                  })}
-                />
-
-      </Stack.Navigator> */}
     </NavigationContainer>
   );
 };
