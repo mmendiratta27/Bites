42af0700260c291bc8cf64d50b99e75dd53dfa87
Manan Mendiratta <123344066+mmendiratta27@users.noreply.github.com>
2023-06-26 09:50:43 -0700

    Update homeScreen.js

--- src/home/homeScreen.js
+++ src/home/homeScreen.js
@@ -1,6 +1,7 @@
 
 import { useState, useRef, useEffect } from "react";
 import { View, ScrollView, SafeAreaView } from "react-native";
+import SIZES from "../../constants";
 
 import Welcome from "../headerInfo/welcome/Welcome";
 import Feed from "../feed/Feed";
