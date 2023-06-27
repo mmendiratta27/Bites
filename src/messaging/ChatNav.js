af3d2466edf89753210e15400b60a994220419ed
jesshuangg <jeshuang@g.hmc.edu>
2023-06-27 14:41:50 -0700

    fixed headers & styled chat home

--- src/messaging/ChatNav.js
+++ src/messaging/ChatNav.js
@@ -3,19 +3,45 @@ import { NavigationContainer } from '@react-navigation/native';
 import ChatScreen from './Chat';
 import ChatHome from './ChatHome';
 
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { View } from "react-native";
 
 const Stack = createStackNavigator();
 
 
-const ChatNav= () => {
+const ChatNav= ({navigation}) => {
     return (
-      <NavigationContainer independent ={true}>
+      <NavigationContainer independent={true}>
         <Stack.Navigator>
-            <Stack.Screen name="ChatHome" component={ChatHome} options={{headerShown: false}}/>
-            <Stack.Screen name='Chat' component={ChatScreen}
-            options={({ route }) => ({ title: route.params.thread.name })}
-            />
-            
+          <Stack.Screen
+            name="ChatHome"
+            component={ChatHome}
+            options={{
+              headerStyle: { backgroundColor: "#F4EEE0" },
+              headerShadowVisible: false,
+              headerTitle: "Group Chats",
+            }}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={ChatScreen}
+            options={({ route }) => ({
+              title: route.params.thread.name,
+              headerStyle: { backgroundColor: "#F4EEE0" },
+              headerShadowVisible: false,
+              headerTitle: "TestChat1",
+              headerLeft: () => (
+                <View style={{ marginLeft: 10 }}>
+                  <Icon
+                    name="chevron-left"
+                    size={30}
+                    color="#353535"
+                    onPress={() => navigation.navigate("GroupChats")}
+                  />
+                </View>
+              ),
+            })}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
